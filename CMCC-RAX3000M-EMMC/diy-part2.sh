75ee04524291d4294cc66230bd43dc82094b9fd4
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-02-24 02:06:22 +0800

    Add files via upload

--- CMCC-RAX3000M-EMMC/diy-part2.sh
+++ CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -11,40 +11,18 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
-
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz -o /tmp/clash1.tar.gz
-curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz -o /tmp/clash2.tar.gz
-
-
-mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
-
-
-tar zxvf /tmp/clash1.tar.gz -C /tmp >/dev/null 2>&1
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
-
-tar zxvf /tmp/clash2.tar.gz -C /tmp >/dev/null 2>&1
-mv /tmp/clash-linux-arm64-2023.08.17-13-gdcc8d87 feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
-
+curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/clash.tar.gz -o /tmp/clash.tar.gz
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-
+chmod +x /tmp/clash_meta >/dev/null 2>&1
+chmod +x /tmp/clash_tun >/dev/null 2>&1
+chmod +x /tmp/clash >/dev/null 2>&1
+mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
+mv /tmp/clash_meta feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+mv /tmp/clash_tun feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
-
-chmod +x feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash* >/dev/null 2>&1
-
-
-rm -rf /tmp/*.gz >/dev/null 2>&1
-
-
-curl -sfL -o /feeds/luci/applications/luci-app-openclash/root/etc/openclash Country.mmdb https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
-curl -sfL -o /feeds/luci/applications/luci-app-openclash/root/etc/openclash https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
-curl -sfL -o /feeds/luci/applications/luci-app-openclash/root/etc/openclash https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
-
-
-
-
+rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 ##-----------------Delete DDNS's examples-----------------
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 ##-----------------Manually set CPU frequency for MT7981B-----------------
