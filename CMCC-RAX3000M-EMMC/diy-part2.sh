08ccb6e3610099a302f8d115a5a179b139457967
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-02-25 00:56:26 +0800

    Add files via upload

--- CMCC-RAX3000M-EMMC/diy-part2.sh
+++ CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -14,6 +14,7 @@
 sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
 curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/clash.tar.gz -o /tmp/clash.tar.gz
+
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash_meta >/dev/null 2>&1
 chmod +x /tmp/clash_tun >/dev/null 2>&1
@@ -23,6 +24,12 @@ mv /tmp/clash_meta feeds/luci/applications/luci-app-openclash/root/etc/openclash
 mv /tmp/clash_tun feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
 mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
+
+##预置OpenClash内核和GEO数据
+curl -sfL -o /feeds/luci/applications/luci-app-openclash/root/etc/openclash/Country.mmdb https://github.com/alecthw/mmdb_china_ip_list/raw/release/lite/Country.mmdb
+curl -sfL -o /feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoSite.dat https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geosite.dat
+curl -sfL -o /feeds/luci/applications/luci-app-openclash/root/etc/openclash/GeoIP.dat https://github.com/Loyalsoldier/v2ray-rules-dat/raw/release/geoip.dat
+
 ##-----------------Delete DDNS's examples-----------------
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 ##-----------------Manually set CPU frequency for MT7981B-----------------
