ac41252d6382d8d2c18e1f35ebba2cca344d3f7a
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-26 11:29:11 +0800

    Update diy-part1.sh

--- CMCC-RAX3000M-EMMC/diy-part1.sh
+++ CMCC-RAX3000M-EMMC/diy-part1.sh
@@ -14,6 +14,8 @@
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
 
 # Add a feed source
-#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+echo 'src-git lhpakg https://github.com/lhbox1/pakg' >> feeds.conf.default
+echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 git clone https://github.com/messense/aliyundrive-webdav package/messense
