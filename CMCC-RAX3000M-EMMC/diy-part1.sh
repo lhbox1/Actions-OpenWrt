ae90631ff4e713e3e1d797059f5d11d2192e85ba
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-29 12:32:50 +0800

    Update diy-part1.sh

--- CMCC-RAX3000M-EMMC/diy-part1.sh
+++ CMCC-RAX3000M-EMMC/diy-part1.sh
@@ -20,5 +20,10 @@
 # echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall;main' >>feeds.conf.default
 git clone https://github.com/messense/aliyundrive-webdav package/messense
 
+#alist
+rm -rf feeds/packages/net/mosdns
+rm -rf feeds/packages/net/alist
+git clone https://github.com/sbwml/luci-app-alist package/alist
+
 # git clone https://github.com/fw876/helloworld package/helloworld
 # git clone https://github.com/xiaorouji/openwrt-passwall package/openwrt-passwall
