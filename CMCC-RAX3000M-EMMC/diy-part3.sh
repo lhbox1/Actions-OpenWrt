33f28838ff12f067171750f28a3a584e6bcc28f4
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-03-30 12:44:38 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -35,7 +35,7 @@ sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/
 
 ##添加软件包
 git clone https://github.com/lhbox1/luci-app-adguardhome package/luci-app-adguardhome
-
+git clone https://github.com/shuishihan/luci-app-easymesh.git package/feeds/luci/luci-app-easymesh
 #argon主题
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 #git clone https://github.com/jerrykuku/luci-app-argon-config package/luci-app-argon-config
