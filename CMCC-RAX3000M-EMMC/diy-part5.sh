f0dd5b64c613c8891b2896ff1fa30e9bdf51c697
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-04-08 01:06:06 +0800

    Update diy-part5.sh

--- CMCC-RAX3000M-EMMC/diy-part5.sh
+++ CMCC-RAX3000M-EMMC/diy-part5.sh
@@ -13,7 +13,7 @@
 #删除冲突插件
 #rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\|passwall\|ssr-plus\).*")
 
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|ssr-plus\|easymesh\|alist\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(quickstart\|openclash\|passwall\|ssr-plus\|easymesh\|alist\).*")
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
