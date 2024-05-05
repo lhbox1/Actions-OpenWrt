1df23fdfb20256ce062ac4a96ebec9a892e02e83
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-05 16:06:54 +0800

    Update diy-part5.sh

--- CMCC-RAX3000M-EMMC/diy-part5.sh
+++ CMCC-RAX3000M-EMMC/diy-part5.sh
@@ -15,7 +15,7 @@
 
 rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|ssr-plus\|alist\|easymesh\).*")
 
-rm -rf feeds/packages/net/alist
+#rm -rf feeds/packages/net/alist
 
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/lang/golang
@@ -44,12 +44,12 @@ sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/
 ##添加软件包
 git clone https://github.com/lhbox1/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
-git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
+#git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 
-git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
+#git clone https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 #alist
-#git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
+git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
 
 #argon主题
 #git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
