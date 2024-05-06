7e42fcee29666708c1adb44de866d4985b72c91e
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-06 23:19:54 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -21,8 +21,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='RAX3000M_E'/g" package/base-files/files/bin/config_generate
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
+#修改默认IP地址
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.15.1/g" ./package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
 # curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 # tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
@@ -34,6 +34,10 @@ sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_genera
 sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 ##-----------------Manually set CPU frequency for MT7981B-----------------
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
+
+#修改默认时区
+sed -i "s/timezone='.*'/timezone='CST-8'/g" ./package/base-files/files/bin/config_generate
+sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" ./package/base-files/files/bin/config_generate
 
 
 ##添加软件包
