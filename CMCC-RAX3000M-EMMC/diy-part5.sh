f10758af03df86b3fceb63c19c23026558ea3c53
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-06 22:29:13 +0800

    Update diy-part5.sh

--- CMCC-RAX3000M-EMMC/diy-part5.sh
+++ CMCC-RAX3000M-EMMC/diy-part5.sh
@@ -24,8 +24,8 @@ git clone https://github.com/sbwml/packages_lang_golang -b 20.x feeds/packages/l
 #修改默认主机名
 sed -i "s/hostname='.*'/hostname='RAX3000M'/g" package/base-files/files/bin/config_generate
 
-# Modify default IP
-sed -i 's/192.168.1.1/192.168.15.1/g' package/base-files/files/bin/config_generate
+#修改默认IP地址
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.15.1/g" ./package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
 # curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 # tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
