8006beb652d146221b3b297cfe5c8ee07bd2386f
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-13 07:49:36 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -54,7 +54,7 @@ git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-pa
 #git clone https://github.com/lhbox1/pakg.git package/pakggg
 
 git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
-
+git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
 
 #alist
 git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
