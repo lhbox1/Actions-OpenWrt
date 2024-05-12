01304fba8282e47b427e642de857a7497fe0f53c
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-12 14:14:34 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -46,18 +46,18 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 ##添加软件包
 #git clone https://github.com/sirpdboy/luci-app-autotimeset package/luci-app-autotimeset
 git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
-#git clone https://github.com/lhbox1/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/lhbox1/luci-app-adguardhome package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
 
 git clone https://github.com/xiaorouji/openwrt-passwall2.git package/luci-app-passwall2
 
-git clone https://github.com/lhbox1/pakg.git package/pakggg
+#git clone https://github.com/lhbox1/pakg.git package/pakggg
 
 git clone --depth=1 --single-branch --branch "main" https://github.com/xiaorouji/openwrt-passwall.git package/luci-app-passwall
 
 
 #alist
-#git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
+git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
 #git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 
 #
