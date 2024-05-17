2087907a7b5b6710e0ac24b7910ed60d68787c4b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-17 20:07:18 +0800

    Update diy-part5.sh

--- CMCC-RAX3000M-EMMC/diy-part5.sh
+++ CMCC-RAX3000M-EMMC/diy-part5.sh
@@ -13,10 +13,11 @@
 #删除冲突插件
 #rm -rf $(find ./feeds/luci/ -type d -regex ".*\(argon\|design\|openclash\|passwall\|ssr-plus\).*")
 
-rm -rf $(find ./feeds/luci/ -type d -regex ".*\(openclash\|passwall\|ssr-plus\|alist\|easymesh\).*")
+rm -rf $(find ./feeds/luci/ -type d -regex ".*\(ddns-go\|openclash\|passwall\|ssr-plus\|alist\|easymesh\).*")
 
 #rm -rf feeds/packages/net/alist
 #rm -rf feeds/packages/net/adguardhome
+rm -rf feeds/packages/net/ddns-go
 
 #rm -rf package/istore
 
@@ -47,6 +48,7 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 ##添加软件包
 git clone https://github.com/lhbox1/luci-app-adguardhome1 package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
+git clone https://github.com/sirpdboy/luci-app-ddns-go package/luci-app-ddns-go
 #git clone https://github.com/sbwml/luci-app-alist package/luci-app-alist
 #git clone https://github.com/lhbox1/pakg.git package/pakggg
 
