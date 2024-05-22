b72722ed979343740c3aa5b549679fc5d66689af
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-05-22 15:03:00 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -15,6 +15,9 @@
 
 rm -rf $(find ./feeds/luci/ -type d -regex ".*\(ddns-go\|openclash\|ssr-plus\|passwall\|alist\|easymesh\).*")
 
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/chinadns-ng
+
 #rm -rf feeds/packages/net/smartdns
 rm -rf feeds/packages/net/ddns-go
 
