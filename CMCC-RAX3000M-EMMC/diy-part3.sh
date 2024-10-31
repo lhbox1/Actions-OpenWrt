1a93d5a1d30d91c8a92dc16347189027211102ef
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-10-31 20:14:04 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -45,6 +45,7 @@ sed -i "/timezone='.*'/a\\\t\t\set system.@system[-1].zonename='Asia/Shanghai'" 
 git clone https://github.com/lhbox1/luci-app-adguardhome1 package/luci-app-adguardhome
 git clone https://github.com/shuishihan/luci-app-easymesh.git package/luci-app-easymesh
 git clone https://github.com/lhbox1/pakg.git package/pakggg
+git clone https://github.com/lhbox1/ipkk.git package/ipkk
 git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
 git clone https://github.com/linkease/istore package/nas-istore
 git clone https://github.com/linkease/nas-packages.git package/nas-istore1
