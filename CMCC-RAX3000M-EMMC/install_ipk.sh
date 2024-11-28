600d01b2751cd01280b1e53fb3ee1c54986d587b
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2024-11-28 19:16:42 +0800

    Update install_ipk.sh

--- CMCC-RAX3000M-EMMC/install_ipk.sh
+++ CMCC-RAX3000M-EMMC/install_ipk.sh
@@ -3,7 +3,6 @@
 
 opkg install /etc/ipk/*.ipk --force-downgrade
 
-sed -i "s/kernel (.*)/kernel (= 5.4.255-1-71d39ab23054c352744e74fe3a674e50)/g" /usr/lib/opkg/status
 sleep 1
 sed -i "s/install_ipk.sh/wan_check.sh/g" /etc/crontabs/root
 
