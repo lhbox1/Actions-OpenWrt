b2e4d62bf0377ccf1234fd6fcc5be305b48f004c
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-04 19:28:15 +0800

    Update diy-part2.sh

--- CMCC-RAX3000M-EMMC/diy-part2.sh
+++ CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -36,5 +36,5 @@ sed -i '/myddns_ipv4/,$d' feeds/packages/net/ddns-scripts/files/etc/config/ddns
 sed -i '/"mediatek"\/\*|\"mvebu"\/\*/{n; s/.*/\tcpu_freq="1.3GHz" ;;/}' package/emortal/autocore/files/generic/cpuinfo
 
 ##添加软件包
-git clone https://github.com/lhbox1/luci-app-adguardhome package/luci-app-adguardhome
+git clone https://github.com/lhbox1/luci-app-adguardhome1 package/luci-app-adguardhome
 
