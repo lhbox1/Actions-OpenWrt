2f03a00d31d2f78e3a40bc1995b4733a88abc26f
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-15 19:51:06 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -18,6 +18,8 @@ rm -rf $(find ./feeds/luci/ -type d -regex ".*\(passwall\|openclash\|ksmbd\|alis
 rm -rf feeds/packages/net/sing-box
 rm -rf feeds/packages/net/trojan-plus
 rm -rf feeds/packages/net/chinadns-ng
+rm -rf feeds/packages/net/xray-core
+rm -rf feeds/packages/net/hysteria
 ##
 rm -rf feeds/packages/net/adguardhome
 rm -rf package/istore
