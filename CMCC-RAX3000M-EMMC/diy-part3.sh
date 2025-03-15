dd5a28ef606f9e3671f3659f5780cef50f807f48
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-15 13:14:43 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -67,7 +67,7 @@ git clone https://github.com/tty228/luci-app-wechatpush package/luci-app-wechatp
 
 git clone https://github.com/lhbox1/openwrt-passwall-packages
 #git clone https://github.com/lmq8267/luci-app-alist package/luci-app-alist
-
+git clon https://github.com/sbwml/luci-app-alist package/luci-app-alist
 git clone https://github.com/linkease/istore package/nas-istore
 git clone https://github.com/linkease/nas-packages.git package/nas-istore1
 git clone https://github.com/linkease/nas-packages-luci.git package/nas-istore2
