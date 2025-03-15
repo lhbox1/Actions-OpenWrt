589ba26c6ee357883c5bfe0f1f30d35f652f921c
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-15 19:27:14 +0800

    Update preset-clash-core.sh

--- CMCC-RAX3000M-EMMC/preset-clash-core.sh
+++ CMCC-RAX3000M-EMMC/preset-clash-core.sh
@@ -44,6 +44,19 @@ cp -r OpenClash-master/luci-app-openclash package/
 rm -rf OpenClash-master
 rm -rf temp.zip
 
+#下载使用master版本passwall
+#wget -qO- https://github.com/xiaorouji/openwrt-passwall/archive/main.zip > temp1.zip
+wget -qO- https://codeload.github.com/xiaorouji/openwrt-passwall/zip/refs/tags/25.3.9-1 > temp1.zip
+unzip -q temp1.zip
+rm -rf feeds/luci/applications/luci-app-passwall
+#cp -r openwrt-passwall-main/luci-app-passwall package/
+
+cp -r openwrt-passwall-25.3.9-1/luci-app-passwall package/
+rm -rf temp1.zip
+#rm -rf openwrt-passwall-main
+rm -rf openwrt-passwall-25.3.9-1
+
+
 #下载使用lucky
 wget  -qO- https://codeload.github.com/gdy666/luci-app-lucky/zip/refs/tags/v2.15.7 > lucky.zip
 unzip -q lucky.zip
