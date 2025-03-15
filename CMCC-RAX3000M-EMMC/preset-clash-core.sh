6a1435eb9f06ed194c734bd2201d4ffd477fe591
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-15 13:41:11 +0800

    Update preset-clash-core.sh

--- CMCC-RAX3000M-EMMC/preset-clash-core.sh
+++ CMCC-RAX3000M-EMMC/preset-clash-core.sh
@@ -12,46 +12,19 @@
 
 # 预置openclash和AdGuardHome内核
 mkdir -p files/etc/openclash/core
-#mkdir -p files/etc/AdGuardHome
-
-# AdGuardHome内核
-#AdGuardHome_URL="https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/AdGuardHome.tar.gz"
-
-
-
 
 #master分支内核
-# dev内核
-CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz"
-# premium内核
-CLASH_TUN_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/premium/clash-linux-arm64-2023.08.17-13-gdcc8d87.gz"
 # Meta内核版本
 CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-arm64.tar.gz"
 
-#自定义
-#curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/clash1.tar.gz -o /tmp/clash.tar.gz
-#tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
-#mv /tmp/clash_meta files/etc/openclash/core/clash_meta >/dev/null 2>&1
-#mv /tmp/clash_tun files/etc/openclash/core/clash_tun >/dev/null 2>&1
-#mv /tmp/clash files/etc/openclash/core/clash >/dev/null 2>&1
-#rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
-
-
-#wget -qO- $CLASH_DEV_URL | tar  xOz > files/etc/openclash/core/clash
-#wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
 wget -qO- $CLASH_META_URL | tar xOz > files/etc/openclash/core/clash_meta
-#wget -qO- $AdGuardHome_URL | tar  xOz > files/etc/AdGuardHome/AdGuardHome
 # 给内核权限
 chmod +x files/etc/openclash/core/clash*
-#chmod +x files/etc/AdGuardHome/AdGuardHome
 # 给wan_check权限
 chmod +x files/etc/wan_check.sh
 chmod +x files/etc/install_ipk.sh
-# meta 要GeoIP.dat 和 GeoSite.dat
-#GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+# GeoSite.dat
 GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
-
-#wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 
 # Country.mmdb
