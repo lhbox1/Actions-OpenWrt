9682049587d8122c02c716e6889a43d135f78ec8
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-03-16 16:28:41 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -33,7 +33,7 @@ rm -rf feeds/packages/net/ddns-go
 sed -i "s/hostname='.*'/hostname='RAX3000M_E'/g" package/base-files/files/bin/config_generate
 
 #修改默认IP地址
-#sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.15.1/g" ./package/base-files/files/bin/config_generate
+sed -i "s/192\.168\.[0-9]*\.[0-9]*/192.168.15.1/g" ./package/base-files/files/bin/config_generate
 ##-----------------Add OpenClash dev core------------------
 # curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-arm64.tar.gz -o /tmp/clash.tar.gz
 # tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
