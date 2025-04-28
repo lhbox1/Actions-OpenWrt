31ae7065959ee0ca410ca91f0bf994a8d9726934
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-04-29 00:22:35 +0800

    Update diy-part2.sh

--- CMCC-RAX3000M-EMMC/diy-part2.sh
+++ CMCC-RAX3000M-EMMC/diy-part2.sh
@@ -17,12 +17,12 @@ curl -sL -m 30 --retry 2 https://raw.githubusercontent.com/lhbox1/Actions-OpenWr
 
 tar zxvf /tmp/clash.tar.gz -C /tmp >/dev/null 2>&1
 chmod +x /tmp/clash_meta >/dev/null 2>&1
-chmod +x /tmp/clash_tun >/dev/null 2>&1
-chmod +x /tmp/clash >/dev/null 2>&1
+#chmod +x /tmp/clash_tun >/dev/null 2>&1
+#chmod +x /tmp/clash >/dev/null 2>&1
 mkdir -p feeds/luci/applications/luci-app-openclash/root/etc/openclash/core
 mv /tmp/clash_meta feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_meta >/dev/null 2>&1
-mv /tmp/clash_tun feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
-mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
+#mv /tmp/clash_tun feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash_tun >/dev/null 2>&1
+#mv /tmp/clash feeds/luci/applications/luci-app-openclash/root/etc/openclash/core/clash >/dev/null 2>&1
 rm -rf /tmp/clash.tar.gz >/dev/null 2>&1
 
 ##预置OpenClash内核和GEO数据
