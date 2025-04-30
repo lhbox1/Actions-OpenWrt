448ae790ae1cca8186e3f0fb4d533054b365da30
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-04-30 12:45:51 +0800

    Update diy-part3.sh

--- CMCC-RAX3000M-EMMC/diy-part3.sh
+++ CMCC-RAX3000M-EMMC/diy-part3.sh
@@ -20,6 +20,22 @@ rm -rf feeds/packages/net/trojan-plus
 rm -rf feeds/packages/net/chinadns-ng
 rm -rf feeds/packages/net/xray-core
 rm -rf feeds/packages/net/hysteria
+rm -rf feeds/packages/net/dns2socks
+rm -rf feeds/packages/net/geoview
+rm -rf feeds/packages/net/ipt2socks
+rm -rf feeds/packages/net/microsocks
+rm -rf feeds/packages/net/naiveproxy
+rm -rf feeds/packages/net/shadowsocks-libev
+rm -rf feeds/packages/net/shadowsocks-rust
+rm -rf feeds/packages/net/shadowsocksr-libev
+rm -rf feeds/packages/net/simple-obfs
+rm -rf feeds/packages/net/tcping
+rm -rf feeds/packages/net/tuic-client
+rm -rf feeds/packages/net/v2ray-geodata
+rm -rf feeds/packages/net/v2ray-plugin
+rm -rf feeds/packages/net/xray-plugin
+
+
 ##
 rm -rf feeds/packages/net/adguardhome
 rm -rf package/istore
