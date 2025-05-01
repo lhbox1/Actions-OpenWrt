14672e8ba8cc4f1df2648f2cebab905a2178d77f
lhbox1 <123519621+lhbox1@users.noreply.github.com>
2025-05-01 21:30:06 +0800

    Update preset-clash-core.sh

--- CMCC-RAX3000M-EMMC/preset-clash-core.sh
+++ CMCC-RAX3000M-EMMC/preset-clash-core.sh
@@ -15,7 +15,7 @@ mkdir -p files/etc/openclash/core
 
 #master分支内核
 # Meta内核版本
-CLASH_META_URL="https://raw.githubusercontent.com/lhbox1/Actions-OpenWrt/main/CMCC-RAX3000M-EMMC/clash_meta.tar.gz"
+CLASH_META_URL="https://github.com/lhbox1/lhatv/raw/main/clash_meta.tar.gz"
 wget -qO- $CLASH_META_URL | tar xOz > files/etc/openclash/core/clash_meta
 # 给内核权限
 chmod +x files/etc/openclash/core/clash*
